c1e839c79bfb5b5e39df985bbc566d98cc0e1285
breno-do-val <brenorvale@gmail.com>
2021-05-07 23:32:07 -0300

    feat(primeiro-componente): adiciona primeiro componente

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Primeiro from './components/basicos/Primeiro';
+
+ReactDOM.render(
+    <div>
+        <Primeiro />
+    </div>, 
+    document.getElementById('root')
+);
